794dc15a0adff893772411239e637fec5bea78e1
Jamison Z Yates <JzYates@usxpress.com>
2021-03-10 12:07:41 -0500

    module #4 done

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,67 @@
+import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
+
+const databaseUrl =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // todo: add initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseUrl,
+      async onConnect(keystone) {
+        console.log('connected to the db');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      // schema items go in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // show ui only for people who pass this test
+      isAccessAllowed: ({ session }) =>
+      // console.log(session);
+
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // graphql query
+      User: 'id name email',
+    }),
+  })
+);
---
+++ sick-fits/backend/schemas/Product.ts
@@ -0,0 +1,41 @@
+import { list } from '@keystone-next/keystone/schema';
+import { text, relationship, select, integer } from '@keystone-next/fields';
+
+export const Product = list({
+  // todo:
+  // access:
+  // ui
+  fields: {
+    name: text({
+      isRequired: true,
+    }),
+    description: text({
+      ui: {
+        displayMode: 'textarea',
+      },
+    }),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
+    status: select({
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'Unavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+    price: integer(),
+    // todo: photo
+  },
+});
